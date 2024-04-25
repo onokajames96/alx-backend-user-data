288ee7796c0adfc7e56e7fd37a2e8f165993932a
onokajames96 <jamesonoka996@gmail.com>
2024-04-25 21:20:20 +0000

    updates

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -3,6 +3,9 @@
 
 import bcrypt
 from db import DB
+from uuid import uuid4
+from typing import Union
+from sqlalchemy.orm.exc import NoResultFound
 from user import User
 
 
@@ -12,18 +15,38 @@ def _hash_password(password: str) -> bytes:
     """
     return bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt())
 
+def _generate_uuid() -> str:
+    """generates uuid4"""
+    return str(uuid4())
+
 
 class Auth:
     """Auth class to interact with the authentication database.
     """
 
     def __init__(self):
+        """Initialization"""
         self._db = DB()
 
     def register_user(self, email: str, password: str) -> User:
+        """register user"""
         try:
             self.db.find_user_by(email=email)
 
         except NoResultFound:
             return self._db.add_user(email, _hash_password(password))
         raise ValueError(f"User {email} already exists")
+
+    def valid_login(self, email: str, password: str) -> bool:
+        """Logins arguments"""
+        try:
+            user = self._db.find_user_by(email=email)
+            if bcrypt.checkpw(password.encode('utf-8'), user.hashed_password):
+                return True
+        except NoResultFound:
+            pass
+        return False
+
+
+    def create_session(self, email: str) -> str:
+
