a30d713ad8a75cc9c1a6d9a89fe35840d5bc8208
onokajames96 <jamesonoka996@gmail.com>
2024-04-25 21:41:23 +0000

    auth

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -2,10 +2,10 @@
 """Auth """
 
 import bcrypt
-from db import DB
 from uuid import uuid4
 from typing import Union
 from sqlalchemy.orm.exc import NoResultFound
+from db import DB
 from user import User
 
 
@@ -14,6 +14,7 @@ def _hash_password(password: str) -> bytes:
     Hash the password using bcrypt.hashpw with the generated salt
     """
     return bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt())
+
 
 def _generate_uuid() -> str:
     """generates uuid4"""
@@ -47,6 +48,32 @@ class Auth:
             pass
         return False
 
-
     def create_session(self, email: str) -> str:
+        """session created"""
+        user = None
+        try:
+            user = self._db.find_user_by(email=email)
+        except NoResultFound:
+            return None
+        if user is None:
+            return None
+        session_id = _generate_uuid()
+        self._db.update_user(user.id, session_id=session_id)
+        return session_id
 
+    def get_user_from_session_id(self, session_id: str) -> Union[User, None]:
+        """Gets user based on the ID"""
+        user = None
+        if session_id is None:
+            return None
+        try:
+            user = self._db.find_user_by(session_id=session_id)
+        except NoResultFound:
+            return None
+        return user
+
+    def destroy_session(self, user_id: int) -> None:
+        """Destroys the seession"""
+        if user_id is None:
+            return None
+        self._db.update_user(user_id, session_id=None)
